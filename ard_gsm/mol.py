071cf5561a5c4e8036743c3b7b81d10ad713a711
Colin Grambow <cgrambow@mit.edu>
2018-05-22 17:19:58 -0400

    Implement Atom, Connection, and MolGraph classes
    MolGraph includes a method that can be used to infer the connections
    in a molecule given the coordinates of the atoms.

--- ard_gsm/mol.py
+++ ard_gsm/mol.py
@@ -5,7 +5,9 @@ import os
 
 import numpy as np
 from rdkit import Chem
-from rdkit.Chem import AllChem
+from rdkit.Chem import AllChem, GetPeriodicTable
+
+_rdkit_periodic_table = GetPeriodicTable()
 
 
 class MolData(object):
@@ -156,3 +158,215 @@ class MolData(object):
             mol = new_mol
 
         return mol
+
+
+class Atom(object):
+    """
+    Represents an atom in a molecular graph.
+    """
+
+    def __init__(self, symbol=None, idx=None, coords=np.array([]), frozen=False):
+        self.symbol = symbol
+        self.idx = idx
+        self.coords = coords
+        self.frozen = frozen
+        self.connections = {}
+
+    def __str__(self):
+        return '{}: {}'.format(self.idx, self.symbol)
+
+    def __repr__(self):
+        return '<Atom "{}">'.format(str(self))
+
+    def copy(self):
+        return Atom(
+            symbol=self.symbol,
+            idx=self.idx,
+            coords=self.coords.copy(),
+            frozen=self.frozen,
+        )
+
+    def get_cov_rad(self):
+        return _rdkit_periodic_table.GetRcovalent(self.symbol)
+
+
+class Connection(object):
+    """
+    Represents a connection in a molecular graph.
+    """
+
+    def __init__(self, atom1, atom2):
+        self.atom1 = atom1
+        self.atom2 = atom2
+
+    def __str__(self):
+        return '({})--({})'.format(str(self.atom1), str(self.atom2))
+
+    def __repr__(self):
+        return '<Connection "{}">'.format(str(self))
+
+    def copy(self):
+        return Connection(self.atom1, self.atom2)
+
+
+class MolGraph(object):
+    """
+    Class to convert coordinates to a molecular graph
+    and to generate driving coordinates.
+
+    Note: Atom indices start at 1.
+    """
+
+    def __init__(self, atoms=None, symbols=None, coords=None):
+        self.atoms = atoms or []
+
+        if not self.atoms and symbols is not None:
+            for idx, symbol in enumerate(symbols):
+                atom = Atom(symbol=symbol, idx=idx+1)
+                self.add_atom(atom)
+
+        if coords is not None:
+            self.set_coords(coords)
+
+    def __iter__(self):
+        for atom in self.atoms:
+            yield atom
+
+    def add_atom(self, atom):
+        self.atoms.append(atom)
+        atom.connections = {}
+        return atom
+
+    def add_connection(self, connection):
+        if connection.atom1 not in self.atoms or connection.atom2 not in self.atoms:
+            raise Exception('Cannot add connection between atoms not in the graph')
+        else:
+            connection.atom1.connections[connection.atom2] = connection
+            connection.atom2.connections[connection.atom1] = connection
+            return connection
+
+    def get_all_connections(self):
+        return list({connection for atom in self.atoms for connection in atom.connections.itervalues()})
+
+    def get_connection(self, atom1, atom2):
+        if atom1 not in self.atoms or atom2 not in self.atoms:
+            raise Exception('One or both of the specified atoms are not in this graph')
+
+        try:
+            return atom1.connections[atom2]
+        except KeyError:
+            raise Exception('The specified atoms are not connected in this graph')
+
+    def remove_atom(self, atom):
+        for atom2 in atom.connections:
+            del atom2.connections[atom]
+        atom.connections = {}
+        self.atoms.remove(atom)
+
+    @staticmethod
+    def remove_connection(connection):
+        del connection.atom1.connections[connection.atom2]
+        del connection.atom2.connections[connection.atom1]
+
+    def copy(self, deep=False):
+        other = MolGraph()
+        atoms = self.atoms
+        mapping = {}
+        for atom in atoms:
+            if deep:
+                atom2 = other.add_atom(atom.copy())
+                mapping[atom] = atom2
+            else:
+                connections = atom.connections
+                other.add_atom(atom)
+                atom.connections = connections
+        if deep:
+            for atom1 in atoms:
+                for atom2 in atom1.connections:
+                    connection = atom1.connections[atom2]
+                    connection = connection.copy()
+                    connection.atom1 = mapping[atom1]
+                    connection.atom2 = mapping[atom2]
+                    other.add_connection(connection)
+        return other
+
+    def merge(self, other):
+        new = MolGraph()
+        for atom in self.atoms:
+            connections = atom.connections
+            new.add_atom(atom)
+            atom.connections = connections
+        for atom in other.atoms:
+            connections = atom.connections
+            new.add_atom(atom)
+            atom.connections = connections
+        return new
+
+    def split(self):
+        new1 = self.copy()
+        new2 = MolGraph()
+
+        if len(self.atoms) == 0:
+            return [new1]
+
+        atoms_to_move = [self.atoms[-1]]
+        idx = 0
+        while idx < len(atoms_to_move):
+            for atom2 in atoms_to_move[idx].connections:
+                if atom2 not in atoms_to_move:
+                    atoms_to_move.append(atom2)
+            idx += 1
+
+        if len(new1.atoms) == len(atoms_to_move):
+            return [new1]
+
+        for atom in atoms_to_move:
+            new2.atoms.append(atom)
+            new1.atoms.remove(atom)
+
+        new = [new2]
+        new.extend(new1.split())
+        return new
+
+    def sort_atoms(self):
+        self.atoms.sort(key=lambda a: a.idx)
+
+    def set_coords(self, coords):
+        """
+        Set atom coordinates. Assumes coords are in same order as self.atoms.
+        """
+        try:
+            coords = np.reshape(coords, (-1,3))
+        except ValueError:
+            raise Exception('Coordinates cannot be reshaped into matrix of size Nx3')
+        assert len(coords) == len(self.atoms)
+
+        for atom, xyz in zip(self.atoms, coords):
+            atom.coords = xyz
+
+    def infer_connections(self):
+        """
+        Delete connections and set them again based on coordinates.
+        """
+        atoms = self.atoms
+
+        for atom in atoms:
+            assert len(atom.coords) != 0
+
+        for atom in atoms:
+            for connection in atom.connections:
+                self.remove_connection(connection)
+
+        sorted_atoms = sorted(atoms, key=lambda a: a.coords[2])
+        for i, atom1 in enumerate(sorted_atoms):
+            for atom2 in sorted_atoms[(i+1):]:
+                crit_dist = (atom1.get_cov_rad() + atom2.get_cov_rad() + 0.45)**2
+                z_boundary = (atom1.coords[2] - atom2.coords[2])**2
+                if z_boundary > 16.0:
+                    break
+                dist_sq = sum((atom1.coords - atom2.coords)**2)
+                if dist_sq > crit_dist or dist_sq < 0.4:
+                    continue
+                else:
+                    connection = Connection(atom1, atom2)
+                    self.add_connection(connection)
