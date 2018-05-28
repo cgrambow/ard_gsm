4f30762620c3536231c0f8ae3cd3196da231ff7f
Colin Grambow <cgrambow@mit.edu>
2018-05-28 19:50:10 -0400

    Add function to read xyz files

--- ard_gsm/util.py
+++ ard_gsm/util.py
@@ -5,6 +5,8 @@ import cPickle as pickle
 import gzip
 import os
 
+import numpy as np
+
 
 def pickle_dump(path, obj, compress=False):
     if compress:
@@ -29,3 +31,30 @@ def iter_sub_dirs(root):
         item = os.path.join(root, item)
         if os.path.isdir(item):
             yield item
+
+
+def read_xyz_file(path, with_energy=False):
+    with open(path) as f:
+        contents = [line.strip() for line in f]
+
+    xyzs = []
+    i = 0
+    while i < len(contents):
+        if not contents[i]:  # Empty line
+            break
+        else:
+            natoms = int(contents[i])
+            xyz = contents[(i+2):(i+2+natoms)]
+            symbols, coords = [], []
+            for line in xyz:
+                data = line.split()
+                symbols.append(data[0])
+                coords.append([float(c) for c in data[1:]])
+            if with_energy:
+                energy = float(contents[i+1])
+                xyzs.append((symbols, np.array(coords), energy))
+            else:
+                xyzs.append((symbols, np.array(coords)))
+            i += natoms + 2
+
+    return xyzs
