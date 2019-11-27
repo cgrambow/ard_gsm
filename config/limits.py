a746dd5b792cd871b4da5c389fa8809a4ca66608
Colin Grambow <cgrambow@mit.edu>
2019-11-27 11:18:39 -0500

    Change default connection limits because radicals are now allowed

--- config/limits.py
+++ config/limits.py
@@ -4,8 +4,8 @@
 # First number is the minimum number of connections;
 # second number is the maximum number of connections.
 connection_limits = {
-    'H': (1, 1),
-    'C': (2, 4),
-    'N': (1, 3),
-    'O': (1, 2),
+    'H': (0, 1),
+    'C': (1, 4),
+    'N': (0, 4),
+    'O': (0, 3),
 }
