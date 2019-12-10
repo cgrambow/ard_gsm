7dca385b108de08767add01e924735e46d1c4cad
Colin Grambow <cgrambow@mit.edu>
2019-12-10 13:00:56 -0500

    Update TS optfreq script

--- scripts/create_ts_optfreq_jobs.py
+++ scripts/create_ts_optfreq_jobs.py
@@ -8,22 +8,30 @@ import re
 
 from ard_gsm.mol import MolGraph
 from ard_gsm.qchem import QChem, QChemError
-from ard_gsm.reaction import group_reactions_by_products, group_reactions_by_connection_changes
+from ard_gsm.reaction import Reaction, group_reactions_by_products, group_reactions_by_connection_changes
 from ard_gsm.util import iter_sub_dirs, read_xyz_file
 
 
 def main():
     args = parse_args()
     num_regex = re.compile(r'\d+')
+    maxnum = float('inf') if args.maxnum is None else args.maxnum
 
-    for prod_sub_dir in iter_sub_dirs(args.prod_dir):
+    for prod_sub_dir in iter_sub_dirs(args.prod_dir, pattern=r'gsm\d+'):
         sub_dir_name = os.path.basename(prod_sub_dir)
+        gsm_num = int(num_regex.search(sub_dir_name).group(0))
+        if gsm_num > maxnum:
+            continue
 
         out_dir = os.path.join(args.out_dir, sub_dir_name)
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
         elif not args.overwrite:
             continue
+
+        qstart_file = os.path.join(args.gsm_dir, sub_dir_name, 'qstart')
+        qtmp = QChem(logfile=qstart_file)
+        charge, multiplicity = qtmp.get_charge(), qtmp.get_multiplicity()  # Doesn't change for products in same folder
 
         print(f'Extracting from {sub_dir_name}...')
 
@@ -34,10 +42,11 @@ def main():
 
             try:
                 qp = QChem(logfile=prod_file)
+                freqs = qp.get_frequencies()
             except QChemError as e:
                 print(e)
                 continue
-            if any(freq < 0.0 for freq in qp.get_frequencies()):
+            if any(freq < 0.0 for freq in freqs):
                 print(f'Ignored {prod_file} because of negative frequency')
                 continue
 
@@ -54,7 +63,7 @@ def main():
             if not args.keep_isomorphic_reactions and reactant.is_isomorphic(product):
                 print(f'Ignored {prod_file} because product is isomorphic with reactant')
                 continue
-            reactions[num] = [reactant, ts, product]
+            reactions[num] = Reaction(reactant, product, ts)
 
         if args.group_by_connection_changes:
             reaction_groups = group_reactions_by_connection_changes(reactions)
@@ -64,13 +73,12 @@ def main():
         for group in reaction_groups:
             # Only consider TS energies instead of "barriers" b/c energies are relative to reactant
             reactions_in_group = list(group.items())  # Make list
-            reactions_in_group.sort(key=lambda r: r[1][1].energy)  # Index 1 is TS
+            reactions_in_group.sort(key=lambda r: r[1].ts.energy)
 
             for num, rxn in reactions_in_group[:args.nextract]:
-                ts = rxn[1]
                 path = os.path.join(out_dir, f'ts_optfreq{num:04}.in')
-                qts = QChem(mol=ts, config_file=args.config)
-                qts.make_input(path)
+                qts = QChem(mol=rxn.ts, config_file=args.config)
+                qts.make_input(path, charge=charge, multiplicity=multiplicity, mem=args.mem)
 
 
 def parse_args():
@@ -78,6 +86,7 @@ def parse_args():
     parser.add_argument('gsm_dir', metavar='GSMDIR', help='Path to directory containing GSM folders')
     parser.add_argument('prod_dir', metavar='PDIR', help='Path to directory containing folders with product opts')
     parser.add_argument('out_dir', metavar='ODIR', help='Path to output directory')
+    parser.add_argument('--mem', type=int, metavar='MEM', help='Q-Chem memory')
     parser.add_argument('-n', '--nextract', type=int, default=4, metavar='N',
                         help='Number of duplicate reactions of the same type to extract (sorted by lowest barrier)')
     parser.add_argument('--group_by_connection_changes', action='store_true',
@@ -85,6 +94,7 @@ def parse_args():
     parser.add_argument('--keep_isomorphic_reactions', action='store_true',
                         help='Consider reactions where the product is isomorphic with the reactant')
     parser.add_argument('--overwrite', action='store_true', help='Overwrite input files in existing directories')
+    parser.add_argument('--maxnum', type=int, metavar='NUM', help='Only make jobs from GSM folders up to this number')
     parser.add_argument(
         '--config', metavar='FILE',
         default=os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, 'config', 'qchem.ts_opt_freq'),
